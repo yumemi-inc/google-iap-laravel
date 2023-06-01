12c4eb596a610bdbe74cd42d2ae2e2eaac38896b
Naoki Ikeguchi <n_ikeguchi@yumemi.co.jp>
2023-06-02 00:13:40 +0900

    fix: Skip RemoveUselessReturnTagRector and UnionTypesRector

--- rector.php
+++ rector.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Quartetcom\StaticAnalysisKit\Rector\Config;
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\Php80\Rector\FunctionLike\UnionTypesRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ParamAnnotationIncorrectNullableRector;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -15,6 +17,8 @@ return static function (RectorConfig $rectorConfig): void {
     ]));
 
     $rectorConfig->skip([
+        RemoveUselessReturnTagRector::class,
+        UnionTypesRector::class,
         ParamAnnotationIncorrectNullableRector::class => [
             __DIR__ . '/src/Internal/Assert.php',
         ],
